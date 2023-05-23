f25334d4f67e99be3bea18622fb7eab8ae8ec060
Nikita Markelov <marknik139@Nikitas-MacBook-Pro.local>
2023-05-23 22:30:21 +0300

    gh-pages add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header () {
             <div className="nav-wrapper">
                 <span className="brand-logo left">Fortnite Shop</span>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="!#">Repo</a></li>
+                    <li><a href="https://github.com/marknik139/fortnite-shop" rel="noreferrer" target="_blank">Repo</a></li>
                 </ul>
             </div>
         </nav>
